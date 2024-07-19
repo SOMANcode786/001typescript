a686f95f3ab4b4905947643ba65bc8104573f8c2
SOMANcode786 <somanamir43@gmail.com>
2024-07-19 11:11:33 -0700

    Add files via upload

---
+++ day 01/qno 1/main.js
@@ -0,0 +1,4 @@
+var myName = "SoMan";
+console.log(myName.toUpperCase());
+console.log(myName.toLowerCase());
+console.log(myName.charAt(0).toLowerCase() + myName.slice(1).toUpperCase());
---
+++ day 01/qno 1/main.ts
@@ -0,0 +1,5 @@
+let myName:string="SoMan";
+
+console.log(myName.toUpperCase());
+console.log(myName.toLowerCase());
+console.log(myName.charAt(0).toLowerCase()+myName.slice(1).toUpperCase());
