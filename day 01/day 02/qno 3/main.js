42f75978313afaed98638c212066cd6578a84b54
SOMANcode786 <somanamir43@gmail.com>
2024-07-19 11:30:40 -0700

    Add files via upload

---
+++ day 01/day 02/qno 3/main.js
@@ -0,0 +1,3 @@
+var myName = "\t\n soman \t\n"; // This saves the name with whitespace
+console.log(myName); // Shows the name with whitespace
+console.log(myName.trim()); // Shows the name without whitespace
