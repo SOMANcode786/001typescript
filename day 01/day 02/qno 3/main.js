d6b905b434beed3775c6d4549afd884d3a1a5f20
SOMANcode786 <somanamir43@gmail.com>
2024-07-19 11:31:50 -0700

    Delete day 01/day 02/qno 3 directory

--- day 01/day 02/qno 3/main.js
+++
@@ -1,3 +0,0 @@
-var myName = "\t\n soman \t\n"; // This saves the name with whitespace
-console.log(myName); // Shows the name with whitespace
-console.log(myName.trim()); // Shows the name without whitespace
--- day 01/day 02/qno 3/main.ts
+++
@@ -1,4 +0,0 @@
-let myName: string = "\t\n soman \t\n"; // This saves the name with whitespace
-
-console.log(myName); // Shows the name with whitespace
-console.log(myName.trim()); // Shows the name without whitespace
