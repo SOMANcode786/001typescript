638c461b7c4724796b15026db2dce0955f1bc65c
SOMANcode786 <somanamir43@gmail.com>
2024-07-22 23:22:24 -0700

    Add files via upload

---
+++ if else condition/main.js
@@ -0,0 +1,10 @@
+var age = 20;
+if (age < 18) {
+    console.log("you are an adult");
+}
+else if (age > 18) {
+    console.log("you are not an adult");
+}
+else {
+    console.log("invalid");
+}
---
+++ if else condition/main.ts
@@ -0,0 +1,16 @@
+let age:number=20;
+
+if(age<18){
+
+console.log("you are an adult");
+
+}
+else if(age>18){
+console.log("you are not an adult")
+
+
+}
+else{
+console.log("invalid");
+
+}
