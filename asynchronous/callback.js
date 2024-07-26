43d5373050f475823d1679c32e165bdf19116d69
SOMANcode786 <somanamir43@gmail.com>
2024-07-26 10:06:46 -0700

    Add files via upload

---
+++ asynchronous/callback.js
@@ -0,0 +1,26 @@
+function washing(callback) {
+    console.log('washing started');
+    setTimeout(function () {
+        console.log("washing done");
+        callback();
+    }, 5000);
+}
+function socking(callback) {
+    console.log('socking started');
+    setTimeout(function () {
+        console.log("soacking done");
+        callback();
+    }, 3000);
+}
+function drying() {
+    console.log('drying started');
+    setTimeout(function () {
+        console.log("drying done");
+    }, 2000);
+}
+washing(() => {
+    socking(() => {
+        drying();
+    });
+});
+export {};
